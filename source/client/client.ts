91b605eadc5e1bd58753323c92ed7ce94dbc691e
Jeremy Bankes <jeremy.bankes@gmail.com>
2022-08-07 12:16:37 -0300

    Added type exports

--- source/client/client.ts
+++ source/client/client.ts
@@ -1,3 +1,7 @@
 export { default as Network } from './Network.js';
 export { default as Dom } from './Dom.js';
 export { default as Cookies } from './Cookies.js';
+
+export * as NetworkTypes from './Network.js';
+export * as DomTypes from './Dom.js';
+export * as CookiesTypes from './Cookies.js';
