ffca109c988a7aa6ef33292cfaf747198fa87b89
Jeremy Bankes <jeremy.bankes@gmail.com>
2023-06-22 23:44:19 -0300

    Exposed new features.

--- source/shared/index.ts
+++ source/shared/index.ts
@@ -1,4 +1,7 @@
+export * from "./Clock";
+export * from "./Color";
 export * from "./ConsoleColor";
 export * from "./Data";
 export * from "./Error";
+export * from "./RegularExpression";
 export * from "./Text";
